fdf3232a9aecd07ed40672275baca6e7687eca83
Shrideep-P <105444736+Shrideep-P@users.noreply.github.com>
2022-06-18 08:00:06 +0530

    Add files via upload

---
+++ landingPage/script.js
@@ -0,0 +1,15 @@
+// Variables
+const toggleButton = document.querySelector('.fa-bars');
+const navbar = document.querySelector('#navbar');
+const xMark = document.querySelector('.fa-xmark')
+
+// Navbar Event Listeners
+toggleButton.addEventListener('click', function toggleBtn() {
+    navbar.classList.toggle('active');
+});
+xMark.addEventListener('click', function toggleBtn() {
+    navbar.classList.toggle('active');
+});
+
+// Our-Works Section
+
